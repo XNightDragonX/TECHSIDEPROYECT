4d669d0eef20b2d5a439aa973877dcda3a25e63e
A5UST3K <89373029+A5UST3K@users.noreply.github.com>
2021-11-26 07:17:25 -0600

    cambio a la clase Tangente

--- app/src/main/java/com/techside/techsidecalculator/tangenteActivity.java
+++ app/src/main/java/com/techside/techsidecalculator/tangenteActivity.java
@@ -15,61 +15,36 @@ public class tangenteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_factorial);
+        setContentView(R.layout.activity_tangente);
         editTextX = findViewById(R.id.editTextNumber);
-        editTextTotal = findViewById(R.id.editTextNumberTotal );
-
+        editTextTotal = findViewById(R.id.editTextNumberTotal);
+        editTextTotal.setEnabled(false);
         editTextX.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if( !hasFocus )
-                {
-                    opStart( );
+                if (!hasFocus) {
+                    realizaOperacion();
                 }
             }
         });
     }
 
-    private void opStart()
-    {
-        double total = 0;
-        if( editTextX.getText().toString() != null ) {
-            if( isNum( editTextX.getText().toString() ) ) {
-                total = Math.tan(Double.parseDouble(editTextX.getText().toString()));
-        }}
-        editTextTotal.setText( String.format("%d" , total ) , TextView.BufferType.SPANNABLE );
-        editTextTotal.setEnabled( false );
-    }
-
-    private int getNum(String decimal)
-    {
-        try
-        {
-            return Integer.parseInt( decimal );
+    public void realizaOperacion() {
+        int aux = editTextX.length();
+        double dNum1 = 0.0;
+        double resultado = 0.0;
+        String arg = editTextX.toString(), sResultado;
+        for (int i = 0; i < aux; i++)
+            dNum1 = dNum1 + arg.charAt(i);
+        double rad = Math.toRadians(dNum1);
+        System.out.println(rad);
+        resultado = Math.tan(rad);
+        System.out.println(resultado);
+        sResultado = Double.toString(resultado);
+        if (dNum1 == 90.0) {
+            editTextTotal.setText("∞");
+        } else {
+            editTextTotal.setText(sResultado);
         }
-        catch( NumberFormatException ex)
-        {
-            ex.printStackTrace();
-        }
-        return 0;
-    }
-
-    private boolean isNum(String decimal)
-    {
-        try
-        {
-            if( decimal == null || decimal.length() == 6)
-            {
-                return false;
-            }
-            Integer.parseInt( decimal );
-            return true;
-        }
-        catch( NumberFormatException ex)
-        {
-            ex.printStackTrace();
-        }
-        return false;
-
     }
 }
