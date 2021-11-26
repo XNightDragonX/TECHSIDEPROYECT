96758a5f826f889ed615a0546a2139e63c967176
Emanuel Pérez Solano <XNightDragonX@outlook.com>
2021-11-25 18:39:15 -0600

    Initial commit

---
+++ TECHSIDECALCULATOR/app/src/main/java/com/techside/techsidecalculator/FactorialActivity.java
@@ -0,0 +1,83 @@
+package com.techside.techsidecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FactorialActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private EditText editTextTotal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factorial);
+        editText = findViewById(R.id.editTextNumber);
+        editTextTotal = findViewById(R.id.editTextNumberTotal );
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if( !hasFocus )
+                {
+                    realizaOperacion( );
+                }
+            }
+        });
+    }
+
+    private void realizaOperacion()
+    {
+        int total = 0;
+        if( editText.getText().toString() != null )
+        {
+            if( isEntero( editText.getText().toString() ) )
+            {
+                total += getEntero( editText.getText().toString() );
+            }
+        }
+        int con = total;
+        while ( con!=0) {
+            total=total*con; con--;
+        }
+        editTextTotal.setText( String.format("%d" , total ) , TextView.BufferType.SPANNABLE );
+        editTextTotal.setEnabled( false );
+    }
+
+    private int getEntero(String entero)
+    {
+        try
+        {
+            return Integer.parseInt( entero );
+        }
+        catch( NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
+    private boolean isEntero(String entero)
+    {
+        try
+        {
+            if( entero == null || entero.length() == 0)
+            {
+                return false;
+            }
+            Integer.parseInt( entero );
+            return true;
+        }
+        catch( NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+}
---
+++ TECHSIDECALCULATOR/app/src/main/java/com/techside/techsidecalculator/MainActivity.java
@@ -0,0 +1,101 @@
+package com.techside.techsidecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void suma(View miView)
+    { }
+    public void resta(View miView)
+    { }
+    public void multiplicacion(View miView)
+    { }
+    public void division(View miView)
+    { }
+    public void factorial(View miView)
+    { }
+
+    private Button Button_suma;
+    private Button Button_resta;
+    private Button Button_division;
+    private Button Button_multiplicacion;
+    private Button Button_Factorial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button_suma = findViewById(R.id.Button_suma);
+        Button_resta = findViewById(R.id.Button_resta);
+        Button_multiplicacion = findViewById(R.id.Button_multiplicacion);
+        Button_division = findViewById(R.id.Button_division);
+        Button_Factorial = findViewById(R.id.Button_Factorial);
+
+        Button_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
+
+        Button_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirResta();
+            }
+        });
+
+        Button_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirMultiplicacion();
+            }
+        });
+
+        Button_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirDivision();
+            }
+        });
+
+        Button_Factorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirFactorial();
+            }
+        });
+
+    }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this , SumaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirResta() {
+        Intent intent = new Intent(this , RestaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirMultiplicacion() {
+        Intent intent = new Intent(this , MultiplicationActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirDivision() {
+        Intent intent = new Intent(this , DivisionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirFactorial() {
+        Intent intent = new Intent(this , FactorialActivity.class);
+        startActivity(intent);
+    }
+
+}
